8949904a2b6e0295df2db09b06a643f573e714d0
TANG ZHIXIONG <zhixiong@momenta.ai>
2021-09-25 08:29:02 +0800

    can enable custom properties

--- include/mapbox/feature.hpp
+++ include/mapbox/feature.hpp
@@ -12,86 +12,6 @@
 namespace mapbox {
 namespace feature {
 
-struct value;
-
-struct null_value_t
-{
-};
-
-constexpr bool operator==(const null_value_t&, const null_value_t&) { return true; }
-constexpr bool operator!=(const null_value_t&, const null_value_t&) { return false; }
-constexpr bool operator<(const null_value_t&, const null_value_t&) { return false; }
-
-constexpr null_value_t null_value = null_value_t();
-
-#define DECLARE_VALUE_TYPE_ACCESOR(NAME, TYPE)        \
-    TYPE* get##NAME() noexcept                        \
-    {                                                 \
-        return match(                                 \
-            [](TYPE& val) -> TYPE* { return &val; },  \
-            [](auto&) -> TYPE* { return nullptr; });  \
-    }                                                 \
-    const TYPE* get##NAME() const noexcept            \
-    {                                                 \
-        return const_cast<value*>(this)->get##NAME(); \
-    }
-// Multiple numeric types (uint64_t, int64_t, double) are present in order to support
-// the widest possible range of JSON numbers, which do not have a maximum range.
-// Implementations that produce `value`s should use that order for type preference,
-// using uint64_t for positive integers, int64_t for negative integers, and double
-// for non-integers and integers outside the range of 64 bits.
-using value_base = mapbox::util::variant<null_value_t, bool, uint64_t, int64_t, double, std::string,
-                                         mapbox::util::recursive_wrapper<std::vector<value>>,
-                                         mapbox::util::recursive_wrapper<std::unordered_map<std::string, value>>>;
-
-struct value : public value_base
-{
-    using array_type = std::vector<value>;
-    using object_type = std::unordered_map<std::string, value>;
-
-    value() : value_base(null_value) {}
-    value(null_value_t) : value_base(null_value) {}
-    value(bool v) : value_base(v) {}
-    value(const char* c) : value_base(std::string(c)) {}
-    value(std::string str) : value_base(std::move(str)) {}
-
-    template <typename T, typename std::enable_if_t<std::is_integral<T>::value, int> = 0,
-              typename std::enable_if_t<std::is_signed<T>::value, int> = 0>
-    value(T t) : value_base(int64_t(t))
-    {
-    }
-
-    template <typename T, typename std::enable_if_t<std::is_integral<T>::value, int> = 0,
-              typename std::enable_if_t<!std::is_signed<T>::value, int> = 0>
-    value(T t) : value_base(uint64_t(t))
-    {
-    }
-
-    template <typename T, typename std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
-    value(T t) : value_base(double(t))
-    {
-    }
-    value(array_type array) : value_base(std::move(array)) {}
-    value(object_type object) : value_base(std::move(object)) {}
-
-    explicit operator bool() const { return !is<null_value_t>(); }
-
-    DECLARE_VALUE_TYPE_ACCESOR(Int, int64_t)
-    DECLARE_VALUE_TYPE_ACCESOR(Uint, uint64_t)
-    DECLARE_VALUE_TYPE_ACCESOR(Bool, bool)
-    DECLARE_VALUE_TYPE_ACCESOR(Double, double)
-    DECLARE_VALUE_TYPE_ACCESOR(Array, array_type)
-    DECLARE_VALUE_TYPE_ACCESOR(Object, object_type)
-    DECLARE_VALUE_TYPE_ACCESOR(String, std::string)
-};
-
-#undef DECLARE_VALUE_TYPE_ACCESOR
-
-using property_map = value::object_type;
-
-// The same considerations and requirement for numeric types apply as for `value_base`.
-using identifier = mapbox::util::variant<null_value_t, uint64_t, int64_t, double, std::string>;
-
 template <class T>
 struct feature
 {
@@ -101,6 +21,26 @@ struct feature
     geometry_type geometry;
     property_map properties;
     identifier id;
+
+// set MAPBOX_GEOMETRY_ENABLE_CUSTOM_PROPERTIES to 1, then you can load
+//      {
+//            "type": "Feature",
+//            "geometry": { "type": "Point", "coordinates": [1, 2] },
+//            "properties": {},
+//            "key": "value",
+//            "list": [],
+//            ...
+//          }
+//      }
+// with custom_properties := {
+//      "key": "value",
+//      "list": [],
+//      ...
+// }
+// just like in geobuf: https://github.com/mapbox/geobuf/blob/master/geobuf.proto#L25
+#if MAPBOX_GEOMETRY_ENABLE_CUSTOM_PROPERTIES
+    property_map custom_properties;
+#endif
 
     feature()
         : geometry(),
@@ -128,12 +68,28 @@ struct feature
         : geometry(std::move(geom_)),
           properties(std::move(prop_)),
           id(std::move(id_)) {}
+#if MAPBOX_GEOMETRY_ENABLE_CUSTOM_PROPERTIES
+    feature(geometry_type const& geom_, property_map const& prop_, identifier const& id_, property_map const &custom_prop_)
+        : geometry(geom_),
+          properties(prop_),
+          id(id_),
+          custom_properties(custom_prop_) {}
+    feature(geometry_type&& geom_, property_map&& prop_, identifier&& id_, property_map&& custom_prop_)
+        : geometry(std::move(geom_)),
+          properties(std::move(prop_)),
+          id(std::move(id_)),
+          custom_properties(std::move(custom_prop_)) {}
+#endif
 };
 
 template <class T>
 constexpr bool operator==(feature<T> const& lhs, feature<T> const& rhs)
 {
+#if MAPBOX_GEOMETRY_ENABLE_CUSTOM_PROPERTIES
+    return lhs.id == rhs.id && lhs.geometry == rhs.geometry && lhs.properties == rhs.properties && lhs.custom_properties == rhs.custom_properties;
+#else
     return lhs.id == rhs.id && lhs.geometry == rhs.geometry && lhs.properties == rhs.properties;
+#endif
 }
 
 template <class T>
@@ -150,12 +106,20 @@ struct feature_collection : Cont<feature<T>>
     using container_type = Cont<feature_type>;
     using size_type = typename container_type::size_type;
 
+#if MAPBOX_GEOMETRY_ENABLE_CUSTOM_PROPERTIES
+    property_map custom_properties;
+    feature_collection() = default;
+    feature_collection& operator=(feature_collection const &other) = default;
+    feature_collection(std::initializer_list<feature_type> args)
+        : container_type(std::move(args)) {}
+#else
     template <class... Args>
     feature_collection(Args&&... args) : container_type(std::forward<Args>(args)...)
     {
     }
     feature_collection(std::initializer_list<feature_type> args)
         : container_type(std::move(args)) {}
+#endif
 };
 
 } // namespace feature
--- include/mapbox/geometry/geometry.hpp
+++ include/mapbox/geometry/geometry.hpp
@@ -12,9 +12,97 @@
 
 // stl
 #include <vector>
+#include <unordered_map>
 
 namespace mapbox {
+
+namespace feature {
+struct value;
+
+struct null_value_t
+{
+};
+
+constexpr bool operator==(const null_value_t&, const null_value_t&) { return true; }
+constexpr bool operator!=(const null_value_t&, const null_value_t&) { return false; }
+constexpr bool operator<(const null_value_t&, const null_value_t&) { return false; }
+
+constexpr null_value_t null_value = null_value_t();
+
+#define DECLARE_VALUE_TYPE_ACCESOR(NAME, TYPE)        \
+    TYPE* get##NAME() noexcept                        \
+    {                                                 \
+        return match(                                 \
+            [](TYPE& val) -> TYPE* { return &val; },  \
+            [](auto&) -> TYPE* { return nullptr; });  \
+    }                                                 \
+    const TYPE* get##NAME() const noexcept            \
+    {                                                 \
+        return const_cast<value*>(this)->get##NAME(); \
+    }
+// Multiple numeric types (uint64_t, int64_t, double) are present in order to support
+// the widest possible range of JSON numbers, which do not have a maximum range.
+// Implementations that produce `value`s should use that order for type preference,
+// using uint64_t for positive integers, int64_t for negative integers, and double
+// for non-integers and integers outside the range of 64 bits.
+using value_base = mapbox::util::variant<null_value_t, bool, uint64_t, int64_t, double, std::string,
+                                         mapbox::util::recursive_wrapper<std::vector<value>>,
+                                         mapbox::util::recursive_wrapper<std::unordered_map<std::string, value>>>;
+
+struct value : public value_base
+{
+    using array_type = std::vector<value>;
+    using object_type = std::unordered_map<std::string, value>;
+
+    value() : value_base(null_value) {}
+    value(null_value_t) : value_base(null_value) {}
+    value(bool v) : value_base(v) {}
+    value(const char* c) : value_base(std::string(c)) {}
+    value(std::string str) : value_base(std::move(str)) {}
+
+    template <typename T, typename std::enable_if_t<std::is_integral<T>::value, int> = 0,
+              typename std::enable_if_t<std::is_signed<T>::value, int> = 0>
+    value(T t) : value_base(int64_t(t))
+    {
+    }
+
+    template <typename T, typename std::enable_if_t<std::is_integral<T>::value, int> = 0,
+              typename std::enable_if_t<!std::is_signed<T>::value, int> = 0>
+    value(T t) : value_base(uint64_t(t))
+    {
+    }
+
+    template <typename T, typename std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+    value(T t) : value_base(double(t))
+    {
+    }
+    value(array_type array) : value_base(std::move(array)) {}
+    value(object_type object) : value_base(std::move(object)) {}
+
+    explicit operator bool() const { return !is<null_value_t>(); }
+
+    DECLARE_VALUE_TYPE_ACCESOR(Int, int64_t)
+    DECLARE_VALUE_TYPE_ACCESOR(Uint, uint64_t)
+    DECLARE_VALUE_TYPE_ACCESOR(Bool, bool)
+    DECLARE_VALUE_TYPE_ACCESOR(Double, double)
+    DECLARE_VALUE_TYPE_ACCESOR(Array, array_type)
+    DECLARE_VALUE_TYPE_ACCESOR(Object, object_type)
+    DECLARE_VALUE_TYPE_ACCESOR(String, std::string)
+};
+
+#undef DECLARE_VALUE_TYPE_ACCESOR
+
+using property_map = value::object_type;
+
+// The same considerations and requirement for numeric types apply as for `value_base`.
+using identifier = mapbox::util::variant<null_value_t, uint64_t, int64_t, double, std::string>;
+}
+
 namespace geometry {
+using value = mapbox::feature::value;
+using null_value_t = mapbox::feature::null_value_t;
+using property_map = mapbox::feature::property_map;
+using identifier = mapbox::feature::identifier;
 
 template <typename T, template <typename...> class Cont = std::vector>
 struct geometry_collection;
@@ -34,6 +122,10 @@ struct geometry : geometry_base<T, Cont>
 {
     using coordinate_type = T;
     using geometry_base<T>::geometry_base;
+
+#if MAPBOX_GEOMETRY_ENABLE_CUSTOM_PROPERTIES
+    property_map custom_properties;
+#endif
 };
 
 template <typename T, template <typename...> class Cont>
